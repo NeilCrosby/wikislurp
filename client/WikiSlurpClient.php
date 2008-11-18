381add2fa323adf31fb2db48a37517520228550c
Neil Crosby <neil@thetenwordreview.com>
2008-11-18 18:15:38 +0000

    Initial commit - just author and license

---
+++ client/WikiSlurpClient.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * A self-contained client class designed to make accessing a WikiSlurp
+ * server easier.
+ *
+ * @author  Neil Crosby <neil@neilcrosby.com>
+ * @license Creative Commons Attribution-Share Alike 3.0 Unported 
+ *          http://creativecommons.org/licenses/by-sa/3.0/
+ **/
+class WikiSlurpClient {
+	
+}
+
+?>
